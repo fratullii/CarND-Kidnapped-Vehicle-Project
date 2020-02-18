f8d77e892d29372ba52146285af0b7a1de932986
Francesco Tullii <francesco.tullii@fcagroup.com>
2020-02-18 14:21:21 +0100

    particle filter method implemented

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -20,18 +20,39 @@
 
 using std::string;
 using std::vector;
+using std::default_random_engine;
+using std::normal_distribution;
 
-void ParticleFilter::init(double x, double y, double theta, double std[]) {
+void ParticleFilter::init(double x, double y, double theta, double std[]){
   /**
    * TODO: Set the number of particles. Initialize all particles to 
-   *   first position (based on estimates of x, y, theta and their uncertainties
-   *   from GPS) and all weights to 1. 
-   * TODO: Add random Gaussian noise to each particle.
-   * NOTE: Consult particle_filter.h for more information about this method 
-   *   (and others in this file).
-   */
-  num_particles = 0;  // TODO: Set the number of particles
+  *   first position (based on estimates of x, y, theta and their uncertainties
+  *   from GPS) and all weights to 1. 
+  * TODO: Add random Gaussian noise to each particle.
+  * NOTE: Consult particle_filter.h for more information about this method 
+  *   (and others in this file).
+  */
 
+  // Define gaussian distributions around gps initial measurements
+  default_random_engine gen;
+  normal_distribution<double> dist_x (x, std[0]);
+  normal_distribution<double> dist_y (y, std[1]);
+  normal_distribution<double> dist_theta (theta, std[2]);
+
+  num_particles = 0;  // TODO: Set the number of particles
+  for (int i = 0; i < num_particles; ++i) {
+    
+    Particle temp_particle;
+    
+    // Sample from the normal ditstribution
+    temp_particle.x = dist_x(gen);
+    temp_particle.y = dist_y(gen);
+    temp_particle.theta = dist_theta(gen);
+    
+    particles.push_back(temp_particle);
+  }
+  
+  is_initialized = true;
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], 
